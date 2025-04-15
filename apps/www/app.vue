72829197ae24f62f27f91cf177d400d2160883a3
Vlad Didyk <vladdidyk1985@gmail.com>
2025-04-15 14:13:54 +0300

    chore: project structure setup

--- apps/www/app.vue
+++ apps/www/app.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="space-y-4">
-    <Button>слава україні</Button>
-    <Button variant="secondary">слава україні</Button>
-    <Button variant="outline">слава україні</Button>
-    <Button variant="link">слава україні</Button>
-    <Button variant="ghost">слава україні</Button>
-    <Button variant="icon" size="icon" rounded="full">
-      <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-      </svg>
-    </Button>
-  </div>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
-<script setup lang="ts">
-  import Button from './components/ui/button/Button.vue';
-</script>
