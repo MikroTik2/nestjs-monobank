67089c4d397db61f742861d824826e188891fd71
Vlad Didyk <vladdidyk1985@gmail.com>
2025-04-16 10:23:04 +0300

    fix: error page template

--- apps/www/error.vue
+++ apps/www/error.vue
@@ -21,20 +21,36 @@ const goBack = () => {
 <template>
   <div class="relative flex flex-col h-[90vh] items-center justify-center">
     <div class="flex flex-col h-full items-center justify-center">
-      <h3 class="text-6xl font-bold mb-6 bg-gradient-to-t from-gray-400 to-white bg-clip-text text-transparent">
+      <h3 class="text-[10rem] font-bold bg-gradient-to-t from-neutral-900 to-white bg-clip-text text-transparent">
         {{ props.error?.statusCode }}
       </h3>
-      <p class="scroll-m-20">
-        {{ props.error?.statusCode === 500 ? 'Internal Server Error. Please try again later.' : 'This page could not be found.' }}
-      </p>
-      <div class="mt-6 flex items-center gap-4">
-        <NuxtLink @click="goBack" :class="cn(buttonVariants({ size: 'lg' }))">
-          Go Back
-        </NuxtLink>
-        <NuxtLink to="/" :class="cn(buttonVariants({variant: 'outline',  size: 'lg' }))">
-          Back To Home
-        </NuxtLink>
-      </div>
+
+      <template v-if="props.error?.statusCode === 500">
+        <h3 class="font-bold text-2xl mb-2">
+          Internal Server Error. Please try again later.
+        </h3>
+        <p class="scroll-m-20 text-center max-w-lg">
+          We are currently experiencing technical difficulties. Our team is working hard to resolve the issue.
+          Please refresh the page or check back after a few minutes.
+        </p>
+      </template>
+
+      <template v-else>
+        <h3 class="font-bold text-2xl mb-2">
+          Something's missing
+        </h3>
+        <p class="scroll-m-20 text-center max-w-lg">
+          Sorry, the page you are looking for doesn't exist or has been moved.
+        </p>
+        <div class="mt-6 flex items-center gap-4">
+          <NuxtLink @click="goBack" :class="cn(buttonVariants({ size: 'lg' }))">
+            Go Back
+          </NuxtLink>
+          <NuxtLink to="/" :class="cn(buttonVariants({ variant: 'outline', size: 'lg' }))">
+            Back To Home
+          </NuxtLink>
+        </div>
+      </template>
     </div>
   </div>
 </template>
