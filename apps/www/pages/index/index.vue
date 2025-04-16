cef452fc4712b4e18c262a8ffc689340ac5508df
Artur <111369356+MikroTik2@users.noreply.github.com>
2025-04-17 01:42:49 +0300

    chore: added animate background

--- apps/www/pages/index/index.vue
+++ apps/www/pages/index/index.vue
@@ -10,9 +10,9 @@ useHead({
 <template>
     <div class="flex justify-center items-center text-center flex-col h-[90vh] max-md: px-4">
         <h1 class="md:text-6xl text-3xl font-semibold">Monobank в NestJS</h1>
-        <p class="mx-auto mt-6 md:max-w-4xl text-sm md:text-md text-muted-foreground">
-            Просте й інтуїтивне рішення для інтеграції платіжної системи Monobank безпосередньо у ваш додаток NestJS. Використовуйте нашу бібліотеку для оперативного створення та
-            керування транзакціями, моніторингу статусів платежів та реалізації інших сценаріїв оплати з максимальною зручністю!
+        <p class="mx-auto mt-6 md:max-w-4xl text-sm md:text-lg text-muted-foreground">
+            Просте та зручне рішення для інтеграції з платіжною системою Monobank у вашому додатку на NestJS. Використовуйте цей модуль для створення та керування рахунками,
+            перевірки статусів, скасування інвойсів і реалізації інших платіжних сценаріїв з максимальним комфортом.
         </p>
         <div class="mt-6 grid w-full min-md:w-[50vw] md:grid-cols-2 gap-6">
             <NuxtLink to="/docs/getting-started/introduction" :class="cn(buttonVariants({ variant: 'outline', size: 'lg' }))">
