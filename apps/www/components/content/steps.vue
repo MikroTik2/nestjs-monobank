a87ebd09bbce0ff279191dc780f8ee2567b51725
Vlad Didyk <vladdidyk1985@gmail.com>
2025-04-22 13:21:51 +0300

    fix: update steps

--- apps/www/components/content/steps.vue
+++ apps/www/components/content/steps.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="step-steps mb-12 ml-4 border-l pl-8">
+    <div class="min-md:step-steps min-md:mb-12 min-md:ml-4 min-md:border-l min-md:pl-8">
         <slot />
     </div>
 </template>
