6175911211dfeeebfb8287743c18e9a56499bf5a
Vlad Didyk <vladdidyk1985@gmail.com>
2025-04-28 17:23:26 +0300

    feat: apdate color in layouts

--- apps/www/layouts/default.vue
+++ apps/www/layouts/default.vue
@@ -5,8 +5,7 @@
             <SidebarNav class="pt-16" />
 
             <SidebarInset>
-                <div style="box-shadow: -20px 30px 400px rgba(56, 155, 255, 0.3)" class="fixed right-[-500px] top-[80px] size-[500px] animate-fadeInOut" />
-                <main class="flex-1 pt-16">
+                <div style="box-shadow: -20px 30px 400px rgba(234, 40, 69, 0.3)" class="fixed right-[-500px] top-[80px] size-[500px] animate-fadeInOut"/>                <main class="flex-1 pt-16">
                     <slot />
                 </main>
             </SidebarInset>
