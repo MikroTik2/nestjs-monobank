feffde77e5c2a0f7cfdf5eb3795b4b1fe7123167
Artur Docenko <111369356+MikroTik2@users.noreply.github.com>
2026-02-02 02:57:51 +0200

    feat: add webhook service to monobank service constructor

--- src/monobank.service.ts
+++ src/monobank.service.ts
@@ -4,6 +4,7 @@ import { MerchantService } from './modules/merchant/merchant.service'
 import { RefundService } from './modules/refund/refund.service'
 import { StatementService } from './modules/statement/statement.service'
 import { WalletService } from './modules/wallet/wallet.service'
+import { WebhookService } from './modules/webhook/webhook.service'
 
 @Injectable()
 export class MonobankService {
@@ -12,6 +13,7 @@ export class MonobankService {
 		public merchants: MerchantService,
 		public refunds: RefundService,
 		public statements: StatementService,
-		public wallets: WalletService
+		public wallets: WalletService,
+		public webhook: WebhookService
 	) {}
 }
